f4af9637b6221a8bf33fb6b976411cb9e374d272
Anastasia Nick <sunrise_6@mail.ru>
2018-12-17 22:52:03 +0300

    Дополнения
    1. Добавлена анимация для формы при ошибке заполнения данных;
    2. Скрипт js вынесен в отдельный файл и подключен;
    3. Исправлены ошибки в отображении сайта в других браузерах.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
-var link = document.querySelector(".modal-open-button");
-var popup = document.querySelector(".modal-search");
-var arrivalDate = popup.querySelector("[name=arrival-date]")
+			var link = document.querySelector(".modal-open-button");
+			var popup = document.querySelector(".modal-search");
+			var form = popup.querySelector("form");
+			var arrivalDate = popup.querySelector("[name=arrival-date]");
+			var departureDate = popup.querySelector("[name=departure-date]");
+			var adultNumber = popup.querySelector("[name=adult-number]");
+			var childrenNumber = popup.querySelector("[name=children-number]");
 
-link.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.toggle("modal-show");
-	arrivalDate.focus();
-})
+			var isStorageSupport = true;
+			var adultNumberStorage = "";
+			var childrenNumberStorage = "";
+
+			try {
+				adultNumberStorage = localStorage.setItem("adult-number");
+				childrenNumberStorage = localStorage.setItem("children-number");
+			} catch (err) {
+				isStorageSupport = false;
+			}
+
+			link.addEventListener("click", function (evt) {
+				evt.preventDefault();
+				popup.classList.toggle("modal-show");
+				form.classList.remove("modal-error");
+				arrivalDate.focus();
+
+				if (adultNumberStorage) {
+					adultNumber.value = adultNumberStorage;
+				} else if (childrenNumberStorage) {
+					childrenNumber.value = childrenNumberStorage;
+				} else {
+					arrivalDate.focus();
+				}
+
+			});
+
+			form.addEventListener("submit", function (evt) {
+				if (!arrivalDate.value || !departureDate.value || !adultNumber.value || !childrenNumber.value) {
+				evt.preventDefault();
+				form.classList.add("modal-error");
+				console.log("Нужно заполнить все поля");
+				} else {
+					if (isStorageSupport) {
+					localStorage.setItem("adult-number", adultNumber.value);
+					localStorage.setItem("children-number", childrenNumber.value);
+					}
+				}
+			});
